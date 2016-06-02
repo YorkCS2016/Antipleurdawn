dacd2896de76b8b8c5202e0ab06102ed4172ff12
Thomas Hickman <Thomas.Hickman42@gmail.com>
2016-06-02 15:31:30 +0100

    Commented the .gitignore file

--- src/net/starbs/antipleurdawn/Main.java
+++ src/net/starbs/antipleurdawn/Main.java
@@ -7,6 +7,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        
         primaryStage.setTitle("Hello World");
         primaryStage.show();
     }
