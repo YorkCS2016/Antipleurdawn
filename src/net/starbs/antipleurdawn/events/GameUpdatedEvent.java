fc49536755559b95e71136999bff877d3ef34d6c
Jack Romo <sharrackor@gmail.com>
2016-06-04 17:29:17 +0100

    Fix implementation of events

--- src/net/starbs/antipleurdawn/events/GameUpdatedEvent.java
+++ src/net/starbs/antipleurdawn/events/GameUpdatedEvent.java
@@ -4,13 +4,16 @@ import javafx.event.Event;
 import net.starbs.antipleurdawn.Piece;
 import net.starbs.antipleurdawn.PlayerType;
 
+import java.util.EventObject;
+
 /**
  * Created by sharr_000 on 6/4/2016.
  */
-public class GameUpdated {
+public class GameUpdatedEvent extends EventObject {
     // TODO
 
-    public GameUpdated() {
+    public GameUpdatedEvent(Object source) {
+        super(source);
         // TODO
     }
 
